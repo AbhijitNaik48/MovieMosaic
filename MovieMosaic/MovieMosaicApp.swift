c8708a4e2062cbcc8e6e3ee3d8916399d7b0f5b5
Abhijit Naik <abhijitnaik481994@gmail.com>
2026-05-01 13:16:36 +0530

    First commit

---
+++ MovieMosaic/MovieMosaicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMosaicApp.swift
+//  MovieMosaic
+//
+//  Created by ABHIJIT NAIK on 01/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieMosaicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
